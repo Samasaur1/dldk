32583684ab22235cda753300bd9d17943861b041
Sam <30577766+Samasaur1@users.noreply.github.com>
2024-05-05 14:20:04 -0700

    Display rewards when playing interactively

--- interactive/__init__.py
+++ interactive/__init__.py
@@ -2,7 +2,17 @@ import gymnasium as gym
 from gymnasium.utils.play import play
 
 def main():
-    play(gym.make("ALE/DonkeyKong-v5", render_mode='rgb_array'))
+    global total_reward
+    total_reward = 0
+    def callback(prev_obs, obs, action, rew, terminated, truncated, info):
+        global total_reward
+        total_reward += rew
+        if rew != 0:
+            print(f"Step reward: {rew}")
+        if terminated:
+            print(f"Total reward: {total_reward}")
+            total_reward = 0
+    play(gym.make("ALE/DonkeyKong-v5", render_mode='rgb_array'), callback=callback)
 
 if __name__ == "__main__":
     main()
