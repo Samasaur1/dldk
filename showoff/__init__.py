93e36753cdf240075287a07eb3d081a3f363e74a
Sam <30577766+Samasaur1@users.noreply.github.com>
2024-05-05 14:21:37 -0700

    Fix showing off

--- showoff/__init__.py
+++ showoff/__init__.py
@@ -2,6 +2,9 @@ import gymnasium as gym
 from gymnasium.utils.play import play
 from sys import argv, exit
 
+import torch
+from torch import nn
+
 def main():
     if len(argv) == 1:
         print("You must choose the strategy to use")
@@ -10,7 +13,15 @@ def main():
 
     file_name = f"{argv[1]}.pt" if len(argv) == 2 else argv[2]
 
-    env = gym.make("ALE/DonkeyKong-v5", render_mode='human')
+    device = 'cpu'
+    if torch.cuda.is_available():
+        device = 'cuda'
+    elif torch.backends.mps.is_available():
+        device = 'mps'
+    print(f"Device: {device}")
+
+    env = gym.make("ALE/DonkeyKong-v5", obs_type="grayscale", render_mode='human')
+    env = gym.wrappers.FrameStack(env, 4)
 
     match argv[1]:
         case "random":
@@ -25,8 +36,52 @@ def main():
             def model(state):
                 return env.action_space.sample()
         case "q":
-            model = QModel()
-            model.load_state_dict(torch.load(file_name))
+            class QNetwork(nn.Module):
+                def __init__(self, input_shape, num_actions):
+                    # print(f"num_actions: {num_actions}")
+                    super().__init__()
+                    self.conv = nn.Sequential(
+                        nn.Conv2d(4, 32, 8, stride=4),
+                        nn.ReLU(),
+                        nn.Conv2d(32, 64, 4, stride=2),
+                        nn.ReLU(),
+                        nn.Conv2d(64, 64, 3, stride=1),
+                        nn.ReLU(),
+                    )
+                    self.fc = nn.Sequential(
+                        nn.Linear(352, 512),
+                        nn.ReLU(),
+                        nn.Linear(512, num_actions),
+                    )
+                    # self.model = nn.Sequential(
+                    #     nn.Conv2d(4, 32, 8, stride=4),
+                    #     nn.ReLU(),
+                    #     nn.Conv2d(32, 64, 4, stride=2),
+                    #     nn.ReLU(),
+                    #     nn.Conv2d(64, 64, 3, stride=1),
+                    #     nn.ReLU(),
+                    #     nn.Flatten(),
+                    #     nn.Linear(352, 512),
+                    #     nn.ReLU(),
+                    #     nn.Linear(512, num_actions),
+                    # )
+
+                def forward(self, x):
+                    # return self.model(x / 255.0)
+                    x = self.conv(x)
+                    x = x.view(-1, 352)
+                    x = self.fc(x)
+                    return x
+            _model = QNetwork(env.observation_space.shape, env.action_space.n).to(device)
+            print("Created model")
+            _model.load_state_dict(torch.load(file_name))
+            print("Loaded state dict")
+            def model(state):
+                qs = _model(torch.Tensor(state).to(device))
+                mx = qs.argmax(dim=1)
+                action = mx.cpu().numpy()
+                return action[0]
+            print("Model function defined")
         case "qq":
             model = DoubleQ()
             model.load_state_dict(torch.load(file_name))
@@ -34,16 +89,26 @@ def main():
             model = Actor()
             model.load_state_dict(torch.load(file_name))
 
+    print("Starting game")
     state, info = env.reset()
+    lives = 4
     reward = 0
     while True:
         action = model(state)
         ns, r, term, trunc, info = env.step(action)
-        state += ns
+        state = ns
         reward += r
+        if r != 0:
+            print(f"Step reward: {r}")
         if term:
-            print(f"Reward: {reward}")
+            print(f"Total reward: {reward}")
             break
+        if info['lives'] != lives:
+            print("Lost life")
+            print("Force jumping to start next life")
+            ns, r, term, trunc, info = env.step(1)
+            lives = info['lives']
+            state = ns
 
 if __name__ == "__main__":
     main()
